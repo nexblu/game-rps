04916c73fd165fb3d296ce8fb061587172786a3f
nexblu <farras.pramudita@gmail.com>
2024-01-22 12:53:23 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,17 @@ const App = () => {
                     <br />
                     <GameRPS></GameRPS>
                     <br /><br />
+                    <div className="row text-center">
+                        <div className="col">
+                            <h3>Kelompok 4: </h3><br />
+                            <p>ANDANA FARRAS PRAMUDITA (50423159)</p><br />
+                            <p>MUHAMMAD RAFI FADILLAH (50423976)</p><br />
+                            <p>NI MADE MAS VIKA AMANDA (51423099)</p><br />
+                            <p>SAMUEL MARTOGI SIHITE (51423357)</p><br />
+                        </div>
+                    </div>
                 </Container>
+                <br />
             </section>
             <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossOrigin></script>
             <script
