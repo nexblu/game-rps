f019f3c304115c8321df5f46f296854c49f1bd6c
nexblu <farras.pramudita@gmail.com>
2024-01-22 07:16:43 +0700

    update

--- src/components/GameRPS.jsx
+++ src/components/GameRPS.jsx
@@ -24,8 +24,20 @@ const GameRPS = () => {
 
         axios.post(url, data, { headers })
             .then(response => {
+                const result_game = response.data['k4']['status']
+                const your_choice = response.data['k4']['your_choice']
+                const bot_choice = response.data['k4']['bot_choice']
                 console.log('Response:', response.data);
-                choiceUpdate(`you ${response.data['k4']['status']}, your choice : ${response.data['k4']['your_choice']} and bot choice ${response.data['k4']['bot_choice']}`)
+                console.log(result)
+                choiceUpdate(
+                    <>
+                        <div className="row text-center fw-bold">
+                            <div className="col result text-lght">
+                                {`you ${result_game}, your choice : ${your_choice} and bot choice ${bot_choice}`}
+                            </div>
+                        </div>
+                    </>
+                )
             })
             .catch(error => {
                 console.error('Error:', error.response.data);
@@ -33,24 +45,20 @@ const GameRPS = () => {
     }
     return (
         <>
-            <div className="row text-center fw-bold">
-                <div className="col result text-lght">
-                    <p>{result}</p>
-                </div>
-            </div>
+            {result}
             <br /><br />
             <div className="row text-center">
-                <div className="col-md rock mb-3 mt-3">
+                <div className="col-md-4 rock mb-3 mt-3">
                     <Image src={Rock} className='mb-3'></Image>
                     <br />
                     <Button variant="primary" onClick={() => buttonClick("rock")}>Rock</Button>
                 </div>
-                <div className="col-md paper mb-3 mt-3">
+                <div className="col-md-4 paper mb-3 mt-3">
                     <Image src={Paper} className='mb-3'></Image>
                     <br />
                     <Button variant="primary" onClick={() => buttonClick("paper")}>Paper</Button>
                 </div>
-                <div className="col-md scissors mb-3 mt-3">
+                <div className="col-md-4 scissors mb-3 mt-3">
                     <Image src={Scissors} className='mb-3'></Image>
                     <br />
                     <Button variant="primary" onClick={() => buttonClick("scissors")}>Scissors</Button>
