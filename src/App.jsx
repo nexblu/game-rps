2d62a48bdef569b8b23add6d545c7c3aaec5a0e9
nexblu <farras.pramudita@gmail.com>
2024-01-22 13:28:05 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
+import { Container, Table } from 'react-bootstrap';
 import GameRPS from './components/GameRPS';
 import '../static/css/main.css'
 
@@ -21,10 +21,37 @@ const App = () => {
                     <div className="row text-center">
                         <div className="col">
                             <h3>Kelompok 4: </h3><br />
-                            <p>ANDANA FARRAS PRAMUDITA (50423159)</p><br />
-                            <p>MUHAMMAD RAFI FADILLAH (50423976)</p><br />
-                            <p>NI MADE MAS VIKA AMANDA (51423099)</p><br />
-                            <p>SAMUEL MARTOGI SIHITE (51423357)</p><br />
+                            <Table striped bordered hover size="sm">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Name</th>
+                                        <th>NPM</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>ANDANA FARRAS PRAMUDITA</td>
+                                        <td>50423159</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>MUHAMMAD RAFI FADILLAH</td>
+                                        <td>50423976</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>NI MADE MAS VIKA AMANDA</td>
+                                        <td>51423099</td>
+                                    </tr>
+                                    <tr>
+                                        <td>4</td>
+                                        <td>SAMUEL MARTOGI SIHITE</td>
+                                        <td>51423357</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
                         </div>
                     </div>
                 </Container>
