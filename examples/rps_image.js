a83d7802da96cc888300707fcfe1cfce76a911c8
nexblu <farras.pramudita@gmail.com>
2024-01-23 22:35:05 +0700

    update

--- examples/rps_image.js
+++ examples/rps_image.js
@@ -1,42 +1,21 @@
 import axios from 'axios'
 
-// const url = 'http://127.0.0.1:8000/api/v1/k4/rps/rps_game';
-
-// const data = {
-//     choice: 'rock'
-// };
-
-// const headers = {
-//     'Accept': 'application/json',
-//     'Content-Type': 'application/json'
-// };
-
-// axios.post(url, data, { headers })
-//     .then(response => {
-//         console.log('Response:', response.data);
-//     })
-//     .catch(error => {
-//         console.error('Error:', error.response.data);
-//     });
-
-// URL endpoint
 const url = 'http://localhost:8000/api/v1/k4/rps/rps_image';
 
-// Header yang dibutuhkan
+
 const headers = {
     'accept': 'application/json',
     'Content-Type': 'application/json',
 };
 
-// Data yang akan dikirim
+
 const data = {
     'choice': 'rock',
 };
 
-// Melakukan permintaan POST dengan Axios
+
 axios.post(url, data, { headers })
     .then(response => {
-        // Mengecek apakah permintaan berhasil (status code 200)
         if (response.status === 200) {
             console.log('Permintaan berhasil!');
             console.log('Response JSON:', response.data);
---
+++ examples/rps_image.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+url = "http://localhost:8000/api/v1/k4/rps/rps_image"
+headers = {
+    "accept": "application/json",
+    "Content-Type": "application/json",
+}
+
+data = {
+    "choice": "rock",
+}
+
+response = requests.post(url, headers=headers, json=data)
+
+response = requests.post(url, headers=headers, data=json.dumps(data))
+
+
+if response.status_code == 200:
+    print("Permintaan berhasil!")
+    print("Response JSON:", response.json())
+else:
+    print("Gagal membuat permintaan. Status code:", response.status_code)
+    print("Response text:", response.text)
