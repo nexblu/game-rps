dbd0bbe9721351fb3ce50b3924488b5861d4ff19
nexblu <farras.pramudita@gmail.com>
2024-01-23 22:38:40 +0700

    update

---
+++ examples/rps_random.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+url = "http://localhost:8000/api/v1/k4/rps/rps_random"
+headers = {
+    "accept": "application/json",
+    "Content-Type": "application/json",
+}
+
+data = {
+    "amount": "2",
+}
+
+response = requests.post(url, headers=headers, data=json.dumps(data))
+
+
+if response.status_code == 200:
+    print("Permintaan berhasil!")
+    print("Response JSON:", response.json())
+else:
+    print("Gagal membuat permintaan. Status code:", response.status_code)
+    print("Response text:", response.text)
